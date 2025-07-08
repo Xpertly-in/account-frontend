6c8f6b90d0d5f90a402158f05f2e43a6cecb7bf3
vishal-grover-dev <vishal.grover.dev@gmail.com>
2025-07-08 13:26:43 +0530

    chore: remove standalone output option from Next.js config
    - Removed the `output: "standalone"` option from `next.config.js` to streamline the configuration.
    - This change aligns with the current project setup and optimizes the build process.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: "standalone",
   images: {
     unoptimized: true,
     domains: ["lh3.googleusercontent.com", "www.google.com"],
